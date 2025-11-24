e43852505baf3c0d1ee7ff010df55514d3db262a
adsonviana <adsonaviana@gmail.com>
2025-11-24 17:30:51 -0300

    Deploy

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,16 +1,20 @@
 "use client"
+import Button from "@/components/button";
 import { Header } from "@/components/Header";
+import ConsultaCard from "@/components/modalNewRegistration";
+import RegistrationForm from "@/components/registerForm";
 
 
 const placeHolderAtedimento = (
-  <div className="p-8 text-center text-gray-400">
-    Conteúdo da Página de Atendimento
+  <div className="p-8 text-center text-gray-400 items-center flex flex-col gap-10">
+    <ConsultaCard/>
+    <Button text="Novo Botão" onClickAction={() => alert("Botão Funcionando!")} />
   </div>
 );
 
 const placeHolderCadastro = (
   <div className="p-8 text-center text-gray-400">
-    Conteúdo da Página de Cadastro
+    <RegistrationForm/>
   </div>
 );
 
