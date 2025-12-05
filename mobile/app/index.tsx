b001514b7be1f7fc9020342eefa126c53b0a6bb0
Santiago <luizphilipsantiago.silvaa@gmail.com>
2025-12-05 18:22:10 -0300

    fix/ mobile base coriguration

--- mobile/app/index.tsx
+++ mobile/app/index.tsx
@@ -1,10 +1,12 @@
 import { View, Text } from "react-native";
 import BaseMobile from "../src/components/mobileBase";
 
+
 const App: React.FC = () => (
   <View className="flex-1 justify-center items-center bg-gray-100">
-    <BaseMobile/>
+    <BaseMobile logo={require("../src/assets/slogan.png")}/>
   </View>
 );
+
 
 export default App;
